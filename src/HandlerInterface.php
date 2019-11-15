c6dfca59692d48c0a09527991804a5592cad3add
Eric Richer <eric.richer@vistoconsulting.com>
2019-11-14 19:46:34 -0500

    Adding HandlerInterface

---
+++ src/HandlerInterface.php
@@ -0,0 +1,23 @@
+<?php
+namespace ZF\Console;
+
+use Zend\Console\Adapter\AdapterInterface;
+
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+interface HandlerInterface
+{
+    
+    /**
+     * Invokable for Zf/Console Route handler
+     * 
+     * @param Route $route
+     * @param AdapterInterface $console
+     */
+    
+    public function __invoke( Route $route, AdapterInterface $console);
+}
+
